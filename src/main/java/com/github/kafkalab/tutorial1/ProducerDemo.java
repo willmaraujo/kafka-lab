5d753fefbe11e5b9caed92468311a78fe3ac8d46
Williams Araújo <crfwilliams@gmail.com>
2021-04-05 17:47:25 +0200

    Introduce a hello world of producer
    Signed-off-by: Williams Araújo <crfwilliams@gmail.com>

--- src/main/java/com/github/kafkalab/tutorial1/ProducerDemo.java
+++ src/main/java/com/github/kafkalab/tutorial1/ProducerDemo.java
@@ -1,8 +1,36 @@
 package com.github.kafkalab.tutorial1;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
 
+    static final String BOOTSTRAP_SERVER = "127.0.0.1:9092";
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // create producer record
+        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "hello world!");
+
+        // send data
+        producer.send(record);
+
+        // flush data
+        producer.flush();
+
+        //flush and close producer
+        producer.close();
     }
 }
